96da6af47951a920acb38bcbef888ecb81a75f00
nicolasizb <nicolasizquierdocontacto@gmail.com>
2023-11-14 17:56:14 -0500

    new log

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+app.use(cors({
+    origin: 'https://eventhub-navy.vercel.app/',
+}))
 app.use(router)
 
 module.exports = {
