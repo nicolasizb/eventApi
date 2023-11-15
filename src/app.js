b49064b38fb975b3f0d84b0d64c0aa8be2c46bef
nicolasizb <nicolasizquierdocontacto@gmail.com>
2023-11-14 19:09:03 -0500

    new log

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(cors())
+app.use(cors( {
+    origin: 'http://localhost:5173'
+}))
 
 app.use(router)
 
