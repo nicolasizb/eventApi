4cc42bf77ead5876dedc282dd3abc717f5afd9c8
nicolasizb <nicolasizquierdocontacto@gmail.com>
2023-11-14 21:30:23 -0500

    new log

--- src/app.js
+++ src/app.js
@@ -6,7 +6,13 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(cors())
+// app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.use(router)
 
