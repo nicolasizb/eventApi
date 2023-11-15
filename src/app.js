01413078624aacdd0a5315fad7a723b4ff8e6aa4
nicolasizb <nicolasizquierdocontacto@gmail.com>
2023-11-14 21:41:46 -0500

    new log

--- src/app.js
+++ src/app.js
@@ -8,12 +8,13 @@ app.use(express.urlencoded({ extended: false }))
 
 // app.use(cors())
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type,Authorization',
+}));
 app.use(router)
 
 module.exports = {
